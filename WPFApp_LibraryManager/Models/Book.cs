86c3e55bf54e0a50065b52da7bab5f32e4d8a61d
gabrielatoader <geea.nicolescu@gmail.com>
2024-10-17 20:35:05 +0300

    Created models to map database entities

---
+++ WPFApp_LibraryManager/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApp_LibraryManager.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public int AuthorId { get; set; }
+        public int PublisherId { get; set; }
+        public int PublishedYear { get; set; }
+        public int CategoryId { get; set; }
+        public string CoverURL { get; set; }
+    }
+}
