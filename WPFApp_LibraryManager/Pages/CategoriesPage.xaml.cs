f45cbda0ba116ea1c387222ef73791f08e63ca2d
gabrielatoader <geea.nicolescu@gmail.com>
2024-11-06 12:37:09 +0200

    feat: created the CategoriesPage UserControl, no backend functionality
    ref: #5

---
+++ WPFApp_LibraryManager/Pages/CategoriesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WPFApp_LibraryManager.Pages
+{
+    public partial class CategoriesPage : UserControl
+    {
+        public CategoriesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
