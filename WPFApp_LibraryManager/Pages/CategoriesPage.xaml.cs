a79ac9cddabbbcfd4d7554497830e742b5a16b15
gabrielatoader <geea.nicolescu@gmail.com>
2024-11-06 12:45:05 +0200

    feat: implemented Category in the navigation bar, fixed previous commit to make solution buildable.

--- WPFApp_LibraryManager/Pages/CategoriesPage.xaml.cs
+++ WPFApp_LibraryManager/Pages/CategoriesPage.xaml.cs
@@ -8,5 +8,40 @@ namespace WPFApp_LibraryManager.Pages
         {
             InitializeComponent();
         }
+
+        private void Clear_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void Save_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void Cancel_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void Edit_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void Delete_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void CategoryList_Dtg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ToDo: implement
+        }
+
+        private void AddCategory_Btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //ToDo: implement
+        }
     }
 }
